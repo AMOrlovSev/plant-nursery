f3164ee7bc6a4770245666734e3bad3968f99ff2
artemorlov <amorlov.sev@gmail.com>
2026-06-03 18:00:10 +0300

    Initial commit

---
+++ src/test/java/sev/amorlov/plant_nursery/PlantNurseryApplicationTests.java
@@ -0,0 +1,13 @@
+package sev.amorlov.plant_nursery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantNurseryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
